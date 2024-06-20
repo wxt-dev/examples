b891a372aff2fd2196499c5ceb8d3c39ed9817fb
Aaron <aaronklinker1@gmail.com>
2024-06-19 21:47:25 -0500

    Disable webextension-polyfill in use-chrome example

--- examples/use-chrome/wxt.config.ts
+++ examples/use-chrome/wxt.config.ts
@@ -1,4 +1,8 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
-export default defineConfig({});
+export default defineConfig({
+  experimental: {
+    includeBrowserPolyfill: false,
+  },
+});
