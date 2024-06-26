b0ff3e184bd8777dbe1fadd46e64e4cb6058687a
Aaron <aaronklinker1@gmail.com>
2024-06-26 11:21:36 -0500

    Add react-content-script-ui example

---
+++ examples/react-content-script-ui/README.md
@@ -0,0 +1,9 @@
+---
+name: React Content Script UI
+description: Basic example of using createShadowRootUi with React.
+---
+
+```sh
+pnpm i
+pnpm dev
+```
---
+++ examples/react-content-script-ui/entrypoints/content/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export default () => {
+  const [count, setCount] = useState(1);
+  const increment = () => setCount((count) => count + 1);
+
+  return (
+    <div>
+      <p>This is React. {count}</p>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+};
