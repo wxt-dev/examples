8714e707010fcb2ee600442f82820551a0734f80
Aaron <aaronklinker1@gmail.com>
2024-09-30 09:52:28 -0500

    Update docs

--- examples/active-tab-content-script/README.md
+++ examples/active-tab-content-script/README.md
@@ -7,3 +7,5 @@ description: Inject a content script only when clicking on the extension action.
 pnpm i
 pnpm dev
 ```
+
+Visit google.com, click the extension's action icon, and the content script will run. Inspect the service worker to see the return value.
--- examples/active-tab-content-script/entrypoints/content.ts
+++ examples/active-tab-content-script/entrypoints/content.ts
@@ -1,9 +1,10 @@
 import { CONTENT_SCRIPT_MATCHES } from "@/utils/matches";
 
 export default defineContentScript({
-  matches: [CONTENT_SCRIPT_MATCHES],
   // Set "registration" to runtime so this file isn't listed in manifest
   registration: "runtime",
+  // URL patterns passed here are automatically added to "host_permissions"
+  matches: [CONTENT_SCRIPT_MATCHES],
 
   async main(ctx) {
     console.log("Content script executed!");
