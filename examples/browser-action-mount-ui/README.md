72cadaead1780075ab2dbb9c82ff597d430e0977
1natsu <1natsu172@users.noreply.github.com>
2024-10-19 01:08:56 +0900

    feat(example): browser-action-mount-ui (#10)
    Co-authored-by: Aaron <aaronklinker1@gmail.com>

---
+++ examples/browser-action-mount-ui/README.md
@@ -0,0 +1,13 @@
+---
+name: Mount UI On Browser Action Click
+description: Click on the extension to mount the UI on the web page.
+apis:
+  - createShadowRootUi
+---
+
+```sh
+pnpm i
+pnpm dev
+```
+
+On any webpage, click the extension icon to mount UI.
---
+++ examples/browser-action-mount-ui/entrypoints/background.ts
@@ -0,0 +1,12 @@
+export default defineBackground(() => {
+  console.log("Hello background!", { id: browser.runtime.id });
+
+  (browser.action ?? browser.browserAction).onClicked.addListener(
+    async (tab) => {
+      console.log("browser action triggered,", tab);
+      if (tab.id) {
+        await browser.tabs.sendMessage(tab.id, { type: "MOUNT_UI" });
+      }
+    },
+  );
+});
