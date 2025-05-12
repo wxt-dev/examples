c4a3e9c4cce17336ad513f0fd3986c406e2bb047
lmdev <138343225+lmdevv@users.noreply.github.com>
2025-05-12 16:48:32 -0400

    add tailwind

--- examples/react-shadcn/wxt.config.ts
+++ examples/react-shadcn/wxt.config.ts
@@ -1,6 +1,10 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
+import tailwindcss from "@tailwindcss/vite";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
+  modules: ["@wxt-dev/module-react"],
+  vite: () => ({
+    plugins: [tailwindcss()],
+  }),
 });
